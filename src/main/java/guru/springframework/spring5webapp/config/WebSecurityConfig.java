8c10ab60c3b579f52bd3247d22d56b82b3f1bf37
Thiago Cavalcante <thiago.kun@gmail.com>
2021-08-25 19:47:05 -0300

    Prepare app for deployment on Heroku
    According to a post on Heroku Dev Center.
    
    Changes:
    - Force the use of HTTPS with the WebSecurityConfig
    - Rate-limit requests with Bucket4j
    - Specify a Java version for Heroku using system.properties file
    - Add Heroku Maven Plugin to pom

---
+++ src/main/java/guru/springframework/spring5webapp/config/WebSecurityConfig.java
@@ -0,0 +1,19 @@
+package guru.springframework.spring5webapp.config;
+
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableCaching
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.requiresChannel()
+            .requestMatchers(r -> r.getHeader("X-Forwarded-Proto") != null)
+            .requiresSecure();
+    }
+
+}
